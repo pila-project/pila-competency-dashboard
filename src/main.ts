a60ddfcb0781de576e344498dee32ab6e04511c2
Stéphane Magnenat <stephane.magnenat@enlightware.ch>
2024-04-24 10:38:17 +0200

    Show git revision on startup.

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,5 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 
+console.info(`PILA competence dashboard rev ${__GIT_REVISION__}`);
 createApp(App).mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
+import { execSync } from 'child_process';
 import vue from '@vitejs/plugin-vue';
 import eslintPlugin from "@nabla/vite-plugin-eslint";
 import mkcert from'vite-plugin-mkcert';
 
+const gitCommitHash = execSync('git rev-parse HEAD').toString().trimEnd();
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), eslintPlugin(), mkcert({mkcertPath: '/usr/bin/mkcert'})],
+  define: {
+    // Define a global constant for the git revision
+    '__GIT_REVISION__': JSON.stringify(gitCommitHash)
+  },
 });
