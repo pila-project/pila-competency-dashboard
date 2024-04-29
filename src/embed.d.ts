7450c8d6dea582b711f1d228334cc74192450cb2
Stéphane Magnenat <stephane.magnenat@enlightware.ch>
2024-04-29 11:32:13 +0200

    Updated typings to match the documentation.

--- src/embed.d.ts
+++ src/embed.d.ts
@@ -6,7 +6,6 @@ declare module '@knowlearning/agents/browser.js' {
 		provider: string;
 		info?: BrowserAgentAuthInfo;
 	}
-
 	export interface BrowserAgentEnvironment {
 		auth: BrowserAgentAuth;
 		domain: string;
@@ -14,10 +13,24 @@ declare module '@knowlearning/agents/browser.js' {
 		session: string;
 	}
 
+	export interface AgentUploadInfo {
+		name?: string,
+		type?: string,
+		data?: string | ArrayBuffer,
+		id: string,
+		browser?: boolean,
+		accept?: string,
+	}
+
 	export interface BrowserAgentInterface {
-		close(): void;
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		state(ns?: string, user?: string, domain?: string): any;
+		login(provider: string): void;
+		logout(): void;
+		uuid(): string;
+		state(id: string, user?: string, domain?: string): Promise<object>;
+		metadata(id: string, user?: string, domain?: string): Promise<object>;
+		watch(id: string, callback: (update: { state: object }) => void, user?: string, domain?: string): void;
+		upload(info?: AgentUploadInfo): Promise<string>;
+		download(id?: string): Promise<Response>;
 		reset(ns: string): Promise<void>;
 		synced(): Promise<void>;
 		environment(userId?: string): Promise<BrowserAgentEnvironment>;
