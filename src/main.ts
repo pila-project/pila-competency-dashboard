e8ff10c69b1b14fce060cb699645cd335dd23ced
Jason Ford <fordjason@gmail.com>
2024-05-02 13:58:53 -0700

    Expose kl Agent for debugging

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
+import klBrowserAgent from '@knowlearning/agents/browser.js';
+
+//  Expose agent for debugging
+window.Agent = klBrowserAgent
 
 console.info(`PILA competence dashboard rev ${__GIT_REVISION__}`);
 createApp(App).mount('#app');
