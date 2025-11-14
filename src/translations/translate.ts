eae6c097897f8ee8d59aa64e9a3d95015216dff8
Stéphane Magnenat <stephane.magnenat@enlightware.ch>
2025-11-14 20:45:56 +0100

    Fixed too early computation of language settings.

--- src/translations/translate.ts
+++ src/translations/translate.ts
@@ -1,10 +1,12 @@
 import th from './th.ts';
 import settings from '../settings.ts';
 
-export const isLangThai = settings.LANGUAGES?.[0]?.startsWith('th') ?? false;
+export function isLangThai() {
+  return settings.LANGUAGES?.[0]?.startsWith('th') ?? false;
+}
 
 export default function translate(sourceString: string) {
-  if (!isLangThai) {
+  if (!isLangThai()) {
     return sourceString;
   }
   const lowerSourceString = sourceString.toLowerCase();
