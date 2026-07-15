f9c11220c30815adf6cff9784835eba08875b27a
Stéphane Magnenat <stephane.magnenat@enlightware.ch>
2026-07-15 15:13:34 +0200

    Updated links to scoring rules documentation.

--- src/GameToInformationMap.ts
+++ src/GameToInformationMap.ts
@@ -2,10 +2,12 @@ const gameToInformationMap: { [key: string]: string } = {
   // SR – Student Dashboard Star System b778aae0-0f34-11f0-8880-e76a25c7e2da
   // SR – Save our Oceans (Easy)
   "0adb500fa86a5cc6b62ab7ca3680ec64" : "11ae3f00-0f14-11f0-9ab3-19305081d06b",
-  "6f48841b58af5d8aa450ae5f9258f1ec" : "11ae3f00-0f14-11f0-9ab3-19305081d06b",
+  "6f48841b58af5d8aa450ae5f9258f1ec": "11ae3f00-0f14-11f0-9ab3-19305081d06b",
+  "166d275fa3c557af8d7a65bf25d09765": "11ae3f00-0f14-11f0-9ab3-19305081d06b",
   // SR – Save our Oceans (Difficult)
   "e86dfc597c04587b9348952bfdcb76b1" : "32ab8200-0f26-11f0-82d7-25a758f42ea1",
-  "237d32dc23cc59e680ed47ec2e3a4fb5" : "32ab8200-0f26-11f0-82d7-25a758f42ea1",
+  "237d32dc23cc59e680ed47ec2e3a4fb5": "32ab8200-0f26-11f0-82d7-25a758f42ea1",
+  "a3db61211b505754aa157cf892648d8f": "32ab8200-0f26-11f0-82d7-25a758f42ea1",
   // SR - Chirpy's Adventure (Easy)
   "c8c710aa7fee5af189791f64fc8270d6" : "e94664b0-1117-11f0-bb98-750ff9de9d61",
   "f4cb3e4fe72355f58e6b58d458d31205" : "e94664b0-1117-11f0-bb98-750ff9de9d61",
@@ -14,27 +16,39 @@ const gameToInformationMap: { [key: string]: string } = {
   "e8d0ab2438765c1c9dd9962d10444a0c" : "45f28930-111a-11f0-bb98-750ff9de9d61",
   // SR - Chirpy's Adventure (Difficult)
   "376229ad69aa520b9ba8c711c2dbdbb8" : "eb001b40-111a-11f0-bb98-750ff9de9d61",
-  "4a95faed0b105822adc1d90c96c4b645" : "eb001b40-111a-11f0-bb98-750ff9de9d61",
+  "4a95faed0b105822adc1d90c96c4b645": "eb001b40-111a-11f0-bb98-750ff9de9d61",
+  // SR - Chirpy's Adventure customized
   "25a6ac35e1c25713b5fedd0008599a52" : "eb001b40-111a-11f0-bb98-750ff9de9d61",
   // SR – Math Invaders
   "4ef94c7e64a95c27b188a00621053697" : "77757160-111b-11f0-bb98-750ff9de9d61",
-  "5f4092d3956e53c98ccec3da0a2f3925" : "77757160-111b-11f0-bb98-750ff9de9d61",
+  "5f4092d3956e53c98ccec3da0a2f3925": "77757160-111b-11f0-bb98-750ff9de9d61",
+  // SR – Math Invaders customized
+  "3689b9aac42e5a4cb5f048e89cdbf80a": "77757160-111b-11f0-bb98-750ff9de9d61",
   // SR – Magical Mixology
   "7a042193eea054b1b85925e074de9b15" : "dcd76c20-111b-11f0-bb98-750ff9de9d61",
-  "de6a2f6f709e57a1b3aa6358a5a8d0bb" : "dcd76c20-111b-11f0-bb98-750ff9de9d61",
-  "27d432ad29ad5e2c8d1c8ebf60d48c0a" : "dcd76c20-111b-11f0-bb98-750ff9de9d61",
+  "de6a2f6f709e57a1b3aa6358a5a8d0bb": "dcd76c20-111b-11f0-bb98-750ff9de9d61",
+  // SR – Magical Mixology customized
+  "27d432ad29ad5e2c8d1c8ebf60d48c0a": "dcd76c20-111b-11f0-bb98-750ff9de9d61",
 
   // Science
   ///////////
   // SR – Mushroom Mayhem
   "d145b0ae50ad557ebb6e91931ec42ce8" : "41be9190-111c-11f0-bb98-750ff9de9d61",
-  "a1c31030facc58d688a07040e25771db" : "41be9190-111c-11f0-bb98-750ff9de9d61",
+  "a1c31030facc58d688a07040e25771db": "41be9190-111c-11f0-bb98-750ff9de9d61",
+  "6d8afb7cd018586ab361331cbfa6cfd2": "41be9190-111c-11f0-bb98-750ff9de9d61",
   // SR – Gravity
   "7160574dc9ec52938f23011a4bcd698f" : "ba674820-111d-11f0-bb98-750ff9de9d61",
-  "93690465d42f52058c92404e329651be" : "ba674820-111d-11f0-bb98-750ff9de9d61",
+  "93690465d42f52058c92404e329651be": "ba674820-111d-11f0-bb98-750ff9de9d61",
+  "714036bc69c752d391873f1d7b1b7021": "ba674820-111d-11f0-bb98-750ff9de9d61",
   // SR – Paper Planes
   "943efee423d75be3bdf761f1e81c7e37" : "ba928160-111e-11f0-bb98-750ff9de9d61",
-  "ba07adc0589f5f3a990f995cc54b8efd" : "ba928160-111e-11f0-bb98-750ff9de9d61",
+  "ba07adc0589f5f3a990f995cc54b8efd": "ba928160-111e-11f0-bb98-750ff9de9d61",
+  "bf489a1bf21d5519a3b2b385a49acc3d": "ba928160-111e-11f0-bb98-750ff9de9d61",
+
+  // Pethematicians 28d92c80-1b8f-11f0-be2e-d3fcfb182e9e
+  "97c9ab96e9c75cd485d50d0c51fa7004": "28d92c80-1b8f-11f0-be2e-d3fcfb182e9e",
+
+  // SPS: 66501b2e0fa0588e9549e2c280cc1be0
 
   // Incredible Machines
   ///////////////////////
@@ -43,11 +57,7 @@ const gameToInformationMap: { [key: string]: string } = {
   // Level 1
   "candli_editor/incredible_machine1": "68992820-1f5e-11f0-9279-b59599f87f2c",
   // Level 2
-  "candli_editor/incredible_machine2": "eea6a190-1f5e-11f0-9279-b59599f87f2c",
-
-
-  // Pethematicians 28d92c80-1b8f-11f0-be2e-d3fcfb182e9e
-  "97c9ab96e9c75cd485d50d0c51fa7004": "28d92c80-1b8f-11f0-be2e-d3fcfb182e9e",
+  "candli_editor/incredible_machine2": "eea6a190-1f5e-11f0-9279-b59599f87f2c"
 }
 
 export default gameToInformationMap;
