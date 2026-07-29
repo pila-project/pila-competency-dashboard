7b63f2f7d0c26f9213ee86b652c02e762396b916
jasonford <fordjason@gmail.com>
2026-07-29 12:09:22 -0700

    Add incredible machine names

--- src/GameToNameMap.ts
+++ src/GameToNameMap.ts
@@ -14,5 +14,17 @@ export function gameToNameMap(key: string) {
             "กลไกมหัศจรรย์ - ระดับ 2" :
             "Incredible Machines - Level 2"
         ,
+        "incredible_machine0": isLangThai() ?
+            "กลไกมหัศจรรย์ - บทช่วยสอน" :
+            "Incredible Machines - Tutorial"
+        ,
+        "incredible_machine1": isLangThai() ?
+            "กลไกมหัศจรรย์ - ระดับ 1" :
+            "Incredible Machines - Level 1"
+        ,
+        "incredible_machine2": isLangThai() ?
+            "กลไกมหัศจรรย์ - ระดับ 2" :
+            "Incredible Machines - Level 2"
+        ,
     }[key];
 }
